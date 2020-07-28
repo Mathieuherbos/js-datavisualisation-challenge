713fce3106d7229916b33ec2e1cfa647dfa007f1
Mathieu Herbos <mathieu.herbos@gmail.com>
2020-07-28 20:44:44 +0200

    ajout début

---
+++ challenge-js-files/assets/script.js
@@ -0,0 +1,45 @@
+//                      CHART 1
+// étape 5.1 CREER LE FICHIER AU BON ENDROIT DANS LE HTML
+
+/* Premier test ABOVE
+// Obtenir une référence à l'élément dans lequel nous voulons insérer un nouveau noeud
+let parentElement = document.getElementById('table1');
+// Créer un nouvel élément
+let newElement = document.createElement("canvas");
+// Insérer le nouvel élément avant le premier enfant
+parentElement.insertBefore(newElement, parentElement.firstChild);
+*/
+
+// TEST en dessous
+let canvas1Place = document.getElementById("table1");
+let canvas1create = document.createElement("canvas");
+let 
+newElement.setAttribute("width", "800px");
+newElement.setAttribute("height", "400px");
+
+
+//                      CHART 2
+// étape 5.1 CREER LE FICHIER AU BON ENDROIT DANS LE HTML
+
+
+
+//                COMMUN AUX 2 CHARTS
+// Changer la taille du canvas 
+/*
+function resizeCanvasToDisplaySize(canvas) {
+    // look up the size the canvas is being displayed
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+ 
+    // If it's resolution does not match change it
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width;
+      canvas.height = height;
+      return true;
+    }
+ 
+    return false;
+ }
+*/
+
+
