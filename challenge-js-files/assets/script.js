ea5ac78ddedfd2b3c38776a4baf48e02dcc8f3b9
Mathieu Herbos <mathieu.herbos@gmail.com>
2020-07-30 08:27:28 +0200

    ajout étape 5.1

--- challenge-js-files/assets/script.js
+++ challenge-js-files/assets/script.js
@@ -1,45 +1,27 @@
 //                      CHART 1
 // étape 5.1 CREER LE FICHIER AU BON ENDROIT DANS LE HTML
 
-/* Premier test ABOVE
-// Obtenir une référence à l'élément dans lequel nous voulons insérer un nouveau noeud
-let parentElement = document.getElementById('table1');
-// Créer un nouvel élément
-let newElement = document.createElement("canvas");
-// Insérer le nouvel élément avant le premier enfant
-parentElement.insertBefore(newElement, parentElement.firstChild);
+//            FORMULE SEB
+/*let myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+document.getElementById("table1").insertAdjacentHTML("beforebegin", '<canvas id="canvas1" height="400" width="400"></canvas>');
+let canvas1 = document.getElementById ("canvas1");
+            }});
 */
 
-// TEST en dessous
-let canvas1Place = document.getElementById("table1");
-let canvas1create = document.createElement("canvas");
-let 
-newElement.setAttribute("width", "800px");
-newElement.setAttribute("height", "400px");
+var chartOne = document.getElementById('table1'); 
+chartOne.insertAdjacentHTML('beforebegin', '<canvas id="canvas1" height="400" width="1000"></canvas>');
 
 
 //                      CHART 2
 // étape 5.1 CREER LE FICHIER AU BON ENDROIT DANS LE HTML
 
+var chartTwo = document.getElementById('table2'); 
+chartTwo.insertAdjacentHTML('beforebegin', '<canvas id="canvas2" height="400" width="1000"></canvas>');
 
 
 //                COMMUN AUX 2 CHARTS
-// Changer la taille du canvas 
-/*
-function resizeCanvasToDisplaySize(canvas) {
-    // look up the size the canvas is being displayed
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
- 
-    // If it's resolution does not match change it
-    if (canvas.width !== width || canvas.height !== height) {
-      canvas.width = width;
-      canvas.height = height;
-      return true;
-    }
- 
-    return false;
- }
-*/
+
 
 
